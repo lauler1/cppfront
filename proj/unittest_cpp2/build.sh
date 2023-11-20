bc97fb0b4ca7787e647317ac7a756e8586067909
Leandro <auler.leandro@gmail.com>
2023-11-20 21:28:47 +0100

    Initial example of a very simple implementation of a unittest framework in cppfront.

---
+++ proj/unittest_cpp2/build.sh
@@ -0,0 +1,42 @@
+
+# Test if CPPFRONT is initialized
+cppfront -vers > /dev/null
+
+
+if [ $? -eq 0 ]; then
+	echo -e "\nCppfront is already initialized."
+	echo      "================================"
+else
+    echo -e "\nInitializing Cppfront..."
+	echo      "========================"
+	export CPPFRONTDIR="../../cppfront"
+	export PATH="../../cppfront:$PATH"
+    cppfront -vers
+fi
+
+cppfront -a -in $1 tests.cpp2 -o out.cpp
+
+if [ $? -eq 0 ]; then
+	g++ -I$CPPFRONTDIR/include/ -g -std=c++23 out.cpp -o out.out
+
+	if [ $? -eq 0 ]; then
+		echo -e "\nCompilation Ok."
+		echo      "==============="
+	    ./out.out
+	else
+	    echo -e "\nCompilation error."
+	    echo      "=================="
+	fi
+
+else
+    echo -e "\nCppfront error."
+    echo      "==============="
+fi
+
+
+#  -no-c[omparison-checks] Disable mixed-sign comparison safety checks
+#  -no-n[ull-checks]       Disable null safety checks
+#  -no-s[ubscript-checks]  Disable subscript safety checks
+
+#  -fno-e[xceptions]       Disable C++ EH - failed 'as' for 'variant' will assert
+#  -fno-r[tti]             Disable C++ RTTI - using 'as' for '*'/'std::any' will assert
---
+++ proj/unittest_cpp2/readme.md
@@ -0,0 +1,17 @@
+# This is a simple unit test framework written in Cppfront.
+
+
+
+
+## Build
+
+To build run:
+
+```bash
+Mixed mode
+$ . ./build.sh
+or Cpp2 only
+$ . ./build.sh -p
+```
+
+This will tranpile, compile and run.
